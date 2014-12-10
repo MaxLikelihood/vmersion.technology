17d118f2c5a92a016644194745a24b60778a3b00
MaxLikelihood <fan.jin@mail.mcgill.ca>
2014-12-10 13:59:10 -0500

    Try to fix GA on menu start

--- public_html/easteregg/blender/asset/javascript/interactive.js
+++ public_html/easteregg/blender/asset/javascript/interactive.js
@@ -11,9 +11,6 @@ document.createElement('video');document.createElement('audio');document.createE
     document.getElementById('menu_Start').style.display='block';
 		setTimeout(callback, 1000);
     setTimeout(animation, 200);
-    $('#menu_Start').on('click', function() {
-      ga('send', 'event', 'button', 'click', 'menu start');
-    });
   	}
 	function play(){
 		video_player.play();
